39189b84ed0a574aec2ece8c886640530c4452a4
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-11-19 15:01:10 +0600

    update

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.apiOverview ,name='api-overview'),
+    path('', views.apiOverview ,name = 'api-overview'),
     path('task-list/', views.taskList, name = 'task-list'),
-    path('task-detail/<str:pk>', views.taskDetail, name = 'task-detail'),
-    path('task-create', views.taskCreate, name='task-create')
+    path('task-detail/<str:pk>/', views.taskDetail, name = 'task-detail'),
+    path('task-create/', views.taskCreate, name = 'task-create'),
+    path('task-update/<str:pk>/', views.taskUpdate, name = 'task-update'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -34,6 +34,14 @@ def taskDetail(request, pk):
 @api_view(['POST'])
 def taskCreate(request):
     serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
