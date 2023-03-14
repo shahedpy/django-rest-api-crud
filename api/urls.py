ba4eba809032ef2134128e4cf38388aca533a87e
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2023-03-15 01:50:12 +0600

    generic view

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
+from .views import TaskList, TaskDetail, ApiOverview
 from . import views
 
 
 urlpatterns = [
-    path('', views.apiOverview ,name = 'api-overview'),
-    path('task-list/', views.taskList, name = 'task-list'),
-    path('task-detail/<str:pk>/', views.taskDetail, name = 'task-detail'),
-    path('task-create/', views.taskCreate, name = 'task-create'),
-    path('task-update/<str:pk>/', views.taskUpdate, name = 'task-update'),
-    path('task-delete/<str:pk>/', views.taskDelete, name = 'task-delete'),
+    path('tasks/', TaskList.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('', ApiOverview.as_view() ,name = 'api-overview'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,50 +1,42 @@
-from rest_framework.decorators import api_view
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .serializers import TaskSerializer
 from .models import Task
 
 
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List':'/task-list/',
-        'Detail View':'/task-detail/<str:pk>/',
-        'Create':'/task-create/',
-        'Update':'/task-update/<str:pk>',
-        'Delete':'/task-delete/<str:pk>',
+class ApiOverview(generics.GenericAPIView):
+    """
+    API Overview
+    ------------
+    This API allows you to create, read, update, and delete tasks.
 
-    }
-    return Response(api_urls)
+    Endpoints
+    ---------
+    - List all tasks: {base_url}/tasks/ (GET)
+    - Create a new task: {base_url}/tasks/ (POST)
+    - Retrieve a task by ID: {base_url}/tasks/<int:pk>/ (GET)
+    - Update a task by ID: {base_url}/tasks/<int:pk>/ (PUT/PATCH)
+    - Delete a task by ID: {base_url}/tasks/<int:pk>/ (DELETE)
 
-@api_view(['GET'])
-def taskList(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+    """
+    def get(self, request, *args, **kwargs):
+        # Use Django REST Framework's `reverse` function to generate URLs for the endpoints
+        api_urls = {
+            'List': reverse('task-list', request=request),
+            'Create': reverse('task-list', request=request),
+            'Retrieve': reverse('task-detail', args=[1], request=request),
+            'Update': reverse('task-detail', args=[1], request=request),
+            'Delete': reverse('task-detail', args=[1], request=request),
+        }
 
-@api_view(['GET'])
-def taskDetail(request, pk):
-    tasks = Task.objects.get(id=pk)
-    serializer = TaskSerializer(tasks, many=False)
-    return Response(serializer.data)
+        # Return the API URLs as a JSON response
+        return Response(api_urls)
 
-@api_view(['POST'])
-def taskCreate(request):
-    serializer = TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-@api_view(['POST'])
-def taskUpdate(request, pk):
-    task = Task.objects.get(id=pk)
-    serializer = TaskSerializer(instance=task, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def taskDelete(request, pk):
-    task = Task.objects.get(id=pk)
-    task.delete()
-    return Response("Task deleted")
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
